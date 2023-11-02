23a275643d36f91bf53b3ed270af7ab84585d8dc
EthanHunt3r <ethanhunterbeschel@hotmail.com>
2023-11-02 22:23:15 +1000

    added 404 & 500 error handling

--- A3/projectfile/qutickets/__init__.py
+++ A3/projectfile/qutickets/__init__.py
@@ -1,16 +1,16 @@
 #import flask - from the package import class
-from flask import Flask 
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 db = SQLAlchemy()
-
+app = Flask(__name__)
 #create a function that creates a web application
 # a web server will run this web application
 def create_app():
   
-    app = Flask(__name__)  # this is the name of the module/package that is calling this app
+    #app = Flask(__name__)  # this is the name of the module/package that is calling this app
     # Should be set to false in a production environment
     app.debug = True
     app.secret_key = 'somesecretkey'
@@ -52,3 +52,13 @@ def create_app():
     app.register_blueprint(auth.auth_bp)
     
     return app
+ 
+@app.errorhandler(404) 
+# inbuilt function which takes error as parameter 
+def not_found(e): 
+  return render_template("404.html", error=e)
+
+@app.errorhandler(500) 
+# inbuilt function which takes error as parameter 
+def not_found(e): 
+  return render_template("500.html", error=e)
